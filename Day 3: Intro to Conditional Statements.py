a3fbb326e091cb037c181a4dfca2a446ca3ddb07
Ayesha Anwar <42913933+AYESHA1996@users.noreply.github.com>
2020-02-19 18:27:46 +0530

    Update Day 3: Intro to Conditional Statements.py

--- Day 3: Intro to Conditional Statements.py
+++ Day 3: Intro to Conditional Statements.py
@@ -9,25 +9,13 @@ import sys
 
 
 if __name__ == '__main__':
-    N = int(input())
-    if N % 2 !=0:
-        print("Weird")
-    elif N in range(2-5):
-        print("Not Weird")
-    elif N in range(6-20):
-        print("Weird")
-    else:
-        print("Not Weird")            
-
-    
-#Alternate
-
-N=int(input())
-if N%2 !=0:
+ N=int(input())
+if N % 2 !=0:
     print("Weird")
-elif N in range(2-5):
+elif N in range(2,6):
     print("Not Weird")
-elif N in range(6-20):
+elif N in range(6,21):
     print("Weird")
 else:
     print("Not Weird")
+    
