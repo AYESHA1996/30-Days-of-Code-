68712fc6c329fd24cb9ac0b976d4960969d1fb29
Ayesha Anwar <42913933+AYESHA1996@users.noreply.github.com>
2020-02-19 17:55:57 +0530

    Create Day 3: Intro to Conditional Statements.py

---
+++ Day 3: Intro to Conditional Statements.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    N = int(input())
+    if N % 2 !=0:
+        print("Weird")
+    elif N in range(2-5):
+        print("Not Weird")
+    elif N in range(6-20):
+        print("Weird")
+    else:
+        print("Not Weird")            
+
+    
+#Alternate
+
+N=int(input())
+if N%2 !=0:
+    print("Weird")
+elif N in range(2-5):
+    print("Not Weird")
+elif N in range(6-20):
+    print("Weird")
+else:
+    print("Not Weird")
